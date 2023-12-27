0825cef5cbcb9810197be24b130f90f2d662bf6f
dhunisinghani <kd19081998@gmail.com>
2023-12-27 18:09:35 +0530

    Booking Details Added

---
+++ src/api/booking-detail/services/booking-detail.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * booking-detail service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::booking-detail.booking-detail');
